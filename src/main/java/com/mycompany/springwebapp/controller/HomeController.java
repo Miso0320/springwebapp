e5b8d6152f77b363f9de0261ff2773756864473c
Miso0320 <[miso8493@hanmail.net]>
2023-07-18 10:16:52 +0900

    로그 추가

--- src/main/java/com/mycompany/springwebapp/controller/HomeController.java
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -13,7 +13,8 @@ public class HomeController {
 	public String index() {
 		log.info("실행1");
 		log.info("실행2");
-		log.info("실행2");
+		log.info("실행3");
+		log.info("실행5");
 		return "index";
 	}
 }
